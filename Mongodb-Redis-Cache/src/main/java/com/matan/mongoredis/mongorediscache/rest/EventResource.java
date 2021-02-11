e9eea4560d9ffcb35dc5e1e2d09eb8cb68f1180e
matan <matan@matan>
2021-02-11 23:56:21 +0200

    service

--- Mongodb-Redis-Cache/src/main/java/com/matan/mongoredis/mongorediscache/rest/EventResource.java
+++ Mongodb-Redis-Cache/src/main/java/com/matan/mongoredis/mongorediscache/rest/EventResource.java
@@ -2,15 +2,9 @@ package com.matan.mongoredis.mongorediscache.rest;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.data.mongodb.core.FindAndModifyOptions;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,59 +14,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.matan.mongoredis.mongorediscache.model.Event;
-import com.matan.mongoredis.mongorediscache.repo.EventRepository;
+import com.matan.mongoredis.mongorediscache.service.EventService;
 
 @RestController
 @RequestMapping("/mongred")
-@CacheConfig(cacheNames = "redisCache")
 public class EventResource {
 
 	@Autowired
-	private MongoTemplate mongoTemplate;
-
-	@Autowired
-	private EventRepository eventRepository;
+	private EventService eventService;
 
 	@PostMapping("addEvent")
-	public Event saveEvent(@RequestBody Event event) {
-		return eventRepository.save(event);
+	public Event addEvent(@RequestBody Event event) {
+		return eventService.addEvent(event);
 	}
 
 	@GetMapping("getOneEvent/{reporterId}")
 	@Cacheable(value = "event", key = "#reporterId", condition = "#reporterId > 0")
 	public Event getOneEvent(@PathVariable int reporterId) {
-		return eventRepository.findById(reporterId).get();
+		return eventService.getOneEvent(reporterId);
 	}
 
 	@GetMapping("getAllEvents")
 	public List<Event> getAllEvents() {
-		return eventRepository.findAll();
+		return eventService.getAllEvents();
 	}
 
 	@PutMapping("updateEvent")
 	@CachePut(value = "event", key = "#reporterId")
 	public Event updateEvent(@RequestBody Event event) {
-		return eventRepository.save(event);
+		return eventService.updateEvent(event);
 	}
 
 	@DeleteMapping("deleteEvent/{reporterId}")
 	@CacheEvict(value = "event", key = "#reporterId")
 	public String deleteEvent(@PathVariable int reporterId) {
-		eventRepository.deleteById(reporterId);
+		eventService.deleteEvent(reporterId);
 		return "deleted event successfully";
 	}
-
-//	@PutMapping("updateByMetricId/{metricId}/{metricValue}")
-//	@CachePut(value = "event", key = "#metricId")
-//	public Event updateByMetricId(@PathVariable(value = "metricId") int metricId,
-//			@PathVariable(value = "metricValue") int metricValue) {
-//
-//		Query query = new Query(Criteria.where("metricId").is(metricId));
-//		Update update = new Update().set("metricValue", metricValue);
-//		Event result = mongoTemplate.findAndModify(query, update,
-//				new FindAndModifyOptions().returnNew(true).upsert(false), Event.class);
-//		return result;
-//
-//	}
 
 }
---
+++ Mongodb-Redis-Cache/src/main/java/com/matan/mongoredis/mongorediscache/service/EventService.java
@@ -0,0 +1,36 @@
+package com.matan.mongoredis.mongorediscache.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matan.mongoredis.mongorediscache.model.Event;
+import com.matan.mongoredis.mongorediscache.repo.EventRepository;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository eventRepository;
+
+	public Event addEvent(Event event) {
+		return eventRepository.save(event);
+	}
+
+	public Event updateEvent(Event event) {
+		return eventRepository.save(event);
+	}
+
+	public void deleteEvent(int reporterId) {
+		eventRepository.deleteById(reporterId);
+	}
+
+	public Event getOneEvent(int reporterId) {
+		return eventRepository.findById(reporterId).get();
+	}
+
+	public List<Event> getAllEvents() {
+		return eventRepository.findAll();
+	}
+
+}
